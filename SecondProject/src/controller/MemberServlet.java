2e0c5f37fe4208139afe16ae6cfe2986142d6d1d
pendalq <seobongdok@naver.com>
2018-09-13 13:52:05 +0900

    1
    1

--- SecondProject/src/controller/MemberServlet.java
+++ SecondProject/src/controller/MemberServlet.java
@@ -35,8 +35,7 @@ public class MemberServlet extends HttpServlet {
 	public void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("do Process success");
 		
-		
-		resp.setCharacterEncoding("utf-8");
+		req.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html; charset=utf-8");
 
 		MemberImpl dao = MemberDao.getInstance();
